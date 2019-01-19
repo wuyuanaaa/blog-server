a5b6b2a86bcf63270a1ef534594a29de5fe2f9b9
wuyuanaaa <dao332508553@163.com>
2019-01-19 17:57:53 +0800

    '修改文章数据模型'

--- models/articleModel.js
+++ models/articleModel.js
@@ -3,11 +3,13 @@ var mongoose = require('mongoose');
 var schema = new mongoose.Schema({
   title: String,
   date: Number,
+  lastDate: Number,
   tags: Array,
   readCount: Number,
   abstract: String,
   content: String,
-  mdContent: String
+  mdContent: String,
+  status: Number
 });
 
 module.exports = mongoose.model('Article', schema);
